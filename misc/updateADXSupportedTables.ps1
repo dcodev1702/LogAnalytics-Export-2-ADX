b1121d3cb03a228505b1b25179266368471b0ed4
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-06-25 00:53:38 -0400

    Create updateADXSupportedTables.ps1

---
+++ misc/updateADXSupportedTables.ps1
@@ -0,0 +1,16 @@
+# Initialize an empty array
+$tables = @()
+
+# Read the file line by line
+Get-Content "ADXSupportedTables2.txt" | ForEach-Object {
+    # Add each line to the array
+    $tables += $_.Trim()
+}
+
+# Create an object with the array
+$obj = [PSCustomObject]@{
+    "SupportedTables" = $tables
+}
+
+# Convert the object to JSON and write it to a file
+$obj | ConvertTo-Json -Depth 100 | Set-Content "ADXSupportedTables.json"
